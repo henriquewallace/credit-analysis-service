fdca2a8b1ade1300efcd5f971f42b2682d139ba4
henriquewallace <henrique.wallace@hotmail.com>
2024-06-17 22:13:15 -0300

    first commit

---
+++ src/test/java/com/wallace/creditanalysisservice/CreditAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.creditanalysisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAnalysisServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
