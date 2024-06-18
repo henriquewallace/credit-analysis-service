be8c58cda9c4bd9d874056f0d40f67c104e159ff
henriquewallace <henrique.wallace@hotmail.com>
2024-06-17 22:15:41 -0300

    feat: create Proposal and User domains

---
+++ src/main/java/com/wallace/creditanalysisservice/domain/User.java
@@ -0,0 +1,17 @@
+package com.wallace.creditanalysisservice.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String cpf;
+    private String phoneNumber;
+    private Double income;
+}
